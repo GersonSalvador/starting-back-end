d20fbde4eda048e476e5874c5f16495c490b0a63
GersonSalvador <zecoevane@yahoo.com.br>
2020-06-25 10:44:57 -0300

    First Route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,8 @@
 import { Router } from 'express';
+import appointmaentsRouter from './appointments.routes';
+
 const routes = Router();
+
+routes.use('/appointments', appointmaentsRouter);
+
 export default routes;
