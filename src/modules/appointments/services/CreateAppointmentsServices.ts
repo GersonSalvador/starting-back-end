cad95cd26e53412e18b4fa1b7eabe42f69bb8600
GersonSalvador <gerson@DESKTOP-4PKP3GT.localdomain>
2020-09-12 18:10:08 -0300

    reorganizing modules done

--- src/modules/appointments/services/CreateAppointmentsServices.ts
+++ src/modules/appointments/services/CreateAppointmentsServices.ts
@@ -2,9 +2,9 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
-import Appointment from '../models/Appointments';
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
-import AppError from '../errors/AppError';
+import Appointment from '../infra/typeorm/entities/Appointments';
+import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentsRepository';
+import AppError from '@shared/errors/AppError';
 
 interface Request {
   provider_id: string;
--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import CreateUserService from '../services/CreatUsersService';
-import UpdateUserAvatarService from '../services/UploadUserAvatarService';
+import CreateUserService from '@modules/users/services/CreatUsersService';
+import UpdateUserAvatarService from '@modules/users/services/UploadUserAvatarService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
-import uploadConfig from '../config/upload';
+import uploadConfig from '@config/upload';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
