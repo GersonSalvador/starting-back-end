00e005e1d2d06816b8da5378d0bce253097ec561
GersonSalvador <gerson.salvador@outlook.com>
2021-02-18 19:45:24 -0300

    Test Coverage

--- src/modules/appointments/services/CreateAppointmentsServices.spec.ts
+++ src/modules/appointments/services/CreateAppointmentsServices.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentRepository';
 import CreateAppointmentsService from './CreateAppointmentsServices';
 
@@ -15,5 +16,28 @@ describe('CreateAppointment', () => {
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('12312123');
+  });
+});
+
+describe('CreateAppointment', () => {
+  it('should not be able to create two appointments to the same date', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentsService(
+      fakeAppointmentRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      date: new Date(),
+      provider_id: '12312123',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: new Date(),
+        provider_id: '12312123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
